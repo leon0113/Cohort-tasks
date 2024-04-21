f66d4920b6b85eec5b501fe817d2b06a9bd6a47d
Leon <tahjib.leon399@gmail.com>
2024-04-21 11:11:56 +0600

    useCallaback1

---
+++ week-6/useCallback/1.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useCallback, useState } from "react";
+
+// Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
+
+export default function App() {
+    const [count, setCount] = useState(0);
+
+    // Your code starts here
+    const handleIncrement = useCallback(() => {
+        console.log("increment");
+        setCount(c => c + 1);
+    }, [])
+
+    function handleDecrement() {
+        console.log("decrement");
+        setCount(c => c - 1);
+    }
+    // Your code ends here
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
+        </div>
+    );
+}
+
+const CounterButtons = ({ onIncrement, onDecrement }) => (
+    <div>
+        <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
+    </div>
+);
